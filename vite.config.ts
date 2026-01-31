ef5e4033207b750b67a4637d6c8d5dda10f40e93
shafik29 <shafik06400@hotmail.fr>
2026-01-31 22:45:00 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/project-5/',
+  base: '/TP-Reseau-Triphase-BAC-PRO-2025-2026/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
